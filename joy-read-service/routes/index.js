5e0419322d611644b82002dd3d6239259d97df67
ethanbli <ethanbli@tencent.com>
2025-06-09 11:37:51 +0800

    add mysql

--- joy-read-service/routes/index.js
+++ joy-read-service/routes/index.js
@@ -5,8 +5,8 @@ const router = express.Router()
 /* GET home page. */
 router.get('/', (req, res, next) => {
   // if you want to use json, you can use res.json instead of res.render
-  // res.json({ title: 'Express' });
-  res.render('index', { title: 'Express' })
+  res.json({ title: 'Express' });
+  // res.render('index', { title: 'Express' })
 })
 
 export default router
