c93b3df6519d41fc891857faf0459238f0d314ee
mac <mac@macdeMacBook-Pro.local>
2025-06-11 00:15:19 +0800

    add redis

--- joy-read-service/routes/api/settings.js
+++ joy-read-service/routes/api/settings.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import { Op, where } from 'sequelize'
 import createHttpError from 'http-errors'
 import db from '../../models/index.js'
-import getPagination from '../../utils/pagination.js'
 import { success } from '../../utils/responses.js'
+import { setKey, getKey } from '../../utils/redis.js'
 
 const router = express.Router()
 const { Setting } = db
@@ -13,7 +12,8 @@ const { NotFound } = createHttpError
  * 查询
  */
 const getSetting = async (req) => {
-  const setting = await Setting.findOne()
+  const setting = await Setting.findOne({ logging: console.log })
+  console.info('setting:', setting)
   if (!setting) {
     throw new NotFound(`ID:${id}未找到`)
   }
@@ -42,6 +42,13 @@ const filterBody = (req) => {
  */
 router.get('/', async (req, res) => {
   const setting = await getSetting(req)
+  // const cacheKey = 'setting'
+  // let setting = await getKey(cacheKey)
+
+  // if(!setting) {
+  //   setting = await getSetting(req)
+  //   await setKey(cacheKey, setting)
+  // }
   success(res, '查询成功', { setting })
 })
 
---
+++ joy-read-service/utils/redis.js
@@ -0,0 +1,52 @@
+import { createClient } from 'redis'
+
+let client
+
+const redisClient = async () => {
+  if (client) return
+
+  client = await createClient()
+    .on('error', (err) => console.log('redis error', err))
+    .connect()
+}
+
+/**
+ * 存入数组或对象，并可选地设置过期时间
+ * @param key 键名
+ * @param value 要存储的值
+ * @param ttl 可选，以秒为单位的过期时间，默认不设置
+ */
+
+const setKey = async (key, value, ttl = null) => {
+  if (!client) await redisClient()
+  value = JSON.stringify(value)
+  await client.set(key, value)
+
+  if (ttl !== null) {
+    await client.expire(key, ttl)
+  }
+}
+
+/**
+ * 读取数组或对象
+ * @param key 键名
+ * @returns {Promise<any>} 解析后的JSON对象或数组
+ */
+const getKey = async (key) => {
+  if (!client) await redisClient() // 确保客户端已初始化
+  const value = await client.get(key) // 将获取到的JSON字符串转换回对象
+  return value ? JSON.parse(value) : null // 如果value为空，返回null而不是抛出错误
+}
+
+/**
+ * 清除缓存数据
+ * @param key
+ * @returns {Promise<void>}
+ */
+const delKey = async (key) => {
+  if (!client) await redisClient() // 确保客户端已初始化
+  await client.del(key)
+}
+
+
+export { redisClient, setKey, getKey, delKey }
