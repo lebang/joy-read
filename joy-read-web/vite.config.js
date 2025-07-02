5f93d800e9627a1054d1f6729539bea293e87fde
ethanbli <ethanbli@tencent.com>
2025-07-02 18:27:48 +0800

    构建优化

--- joy-read-web/vite.config.js
+++ joy-read-web/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import devtoolsJosn from 'vite-plugin-devtools-json'
+import Sonda from 'sonda/vite';
 
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -11,9 +12,21 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const pathResolve = (dir) => path.resolve(__dirname, dir)
-
 // https://vite.dev/config/
 export default defineConfig({
+  build: { 
+    chunkSizeWarningLimit: 800,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-libs': ['vue', 'vue-router', 'pinia', 'axios', 'lodash-es', '@element-plus/icons-vue'],
+          'element-plus': ['element-plus'],
+          'fluent-editor': ['quill', '@opentiny/fluent-editor'],
+        },
+      }
+    },
+  },
   server: {
     host: '',
     port: '3001',
@@ -42,12 +55,13 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    // legacy({
-    //   targets: ['ie >= 11', 'chrome 52', 'Android 4.1', 'iOS 7.1'],
-    //   modernPolyfills: true,
-    //   additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    // }),
+    legacy({
+      targets: ['ie >= 11', 'chrome 52', 'Android 4.1', 'iOS 7.1'],
+      modernPolyfills: true,
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
     devtoolsJosn(),
+    Sonda(),
   ],
   css: {
     preprocessorOptions: {
