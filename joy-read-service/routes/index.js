ea25501b7d961f2788d3d657e253fe20b52b05fa
ethanbli <ethanbli@tencent.com>
2025-07-18 16:00:52 +0800

    add device-detector

--- joy-read-service/routes/index.js
+++ joy-read-service/routes/index.js
@@ -1,11 +1,24 @@
 import express from 'express'
+import DeviceDetector from "node-device-detector";
+import DeviceHelper from "node-device-detector/helper.js";
 
 const router = express.Router()
+const deviceDetector = new DeviceDetector()
 
 router.get('/', (req, res) => {
   const userAgent = req.headers['user-agent'];
   console.log('userAgent:', userAgent);
-  res.send('Hello World!')
+
+  const device = deviceDetector.detect(userAgent);
+  const deviceType = DeviceHelper.getDeviceType(device);
+  const clientType = DeviceHelper.getClientType(device);
+  const ret = {
+    device,
+    deviceType,
+    clientType,
+  }
+  
+  res.send(ret)
 })
 
 export default router
